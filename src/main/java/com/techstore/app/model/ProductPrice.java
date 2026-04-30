077fe286af18f3316185834ea1ae5e2f8000bc0f
ThomasMumladze <mumladzea4@gmail.com>
2026-04-30 20:21:09 +0400

    dev

--- src/main/java/com/techstore/app/model/ProductPrice.java
+++ src/main/java/com/techstore/app/model/ProductPrice.java
@@ -4,11 +4,9 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 // @Entity
