bda892507686ad9b09fdf467613778bc7e3a51d2
ThomasMumladze <mumladzea4@gmail.com>
2026-04-30 20:06:56 +0400

    add cpu dto

---
+++ src/main/java/com/techstore/app/dto/CpuDto.java
@@ -0,0 +1,25 @@
+package com.techstore.app.dto;
+
+import com.techstore.app.model.ProductPrice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class CpuDto {
+    private int cpuId;
+    private String manufacturer;
+    private String type;
+    private String generation;
+    private String model;
+    private String socket;
+    private int cores;
+    private int threads;
+    private String img;
+    private String bodyImage;
+
+    private ProductPrice productPrice = new ProductPrice();
+}
